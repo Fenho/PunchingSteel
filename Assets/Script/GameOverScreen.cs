883bf20383b6c994923d4d050e02fa65b69f17ad
barbaraim <birarrazaval1@uc.cl>
2022-09-06 18:39:25 -0400

    changes from window size and connecting scenes

--- Assets/Script/GameOverScreen.cs
+++ Assets/Script/GameOverScreen.cs
@@ -17,7 +17,7 @@ public class GameOverScreen : MonoBehaviour
 
     public void RestartButton()
     {
-        //SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Scene1");
     }
 
     public void MainMenuButton()
