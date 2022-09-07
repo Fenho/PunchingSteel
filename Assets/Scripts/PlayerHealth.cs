231196fbffdeb2acd01481b94fe67f8ba082c290
JuPaOlivares <jpolivares3@uc.cl>
2022-09-07 18:49:29 -0300

    cambios al sistema de vida :hearth:

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameLogic : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
     public int playerHealth;
-    public int enemyHealth;
+    public int playerMaxHealth = 100;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerHealth = playerMaxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TakeDamage(int damage) {
+        playerHealth -= damage;
     }
 }
