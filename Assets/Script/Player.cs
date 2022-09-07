50addfbbeb9ef692fe67bef2aebd6cc8397c082a
barbaraim <birarrazaval1@uc.cl>
2022-09-07 10:59:45 -0400

    player and enemy's health bars

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float maxHealth = 100;
+    public float currentHealth = 50;
     // Start is called before the first frame update
     void Start()
     {
