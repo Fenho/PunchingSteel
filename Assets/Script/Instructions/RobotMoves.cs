fa0eddb15ba999b66747ccf4366767f26ab8c8dc
barbaraim <birarrazaval1@uc.cl>
2022-09-22 13:22:09 -0300

    (fix) moves to new roles and add sounds

--- Assets/Script/Instructions/RobotMoves.cs
+++ Assets/Script/Instructions/RobotMoves.cs
@@ -14,25 +14,24 @@ public class RobotMoves : MonoBehaviour
     private GameObject trainerGo;
     private TrainerMoves trainer;
 
+    // Music
+    public AudioSource audioSource;
+    public AudioClip punchSound1;
+    public AudioClip punchSound2;
+    public AudioClip dodgeSound1;
+    public AudioClip dodgeSound2;
+    public AudioClip missSound;
+
+    public float volume = 1.0f;
+
     [SerializeField] private float jabSpeed = 0.5f;
 
-    [SerializeField] public string teamState = "idle";
+    [SerializeField] public string teamState = State.IDLE;
 
     public Animator animator;
 
-    // Animation States
-    const string STATE_IDLE = "idle";
-    const string STATE_JAB = "jab";
-    const string STATE_RIGHT = "right";
-    const string STATE_DODGE_LEFT = "dodge-left";
-    const string STATE_DODGE_RIGHT = "dodge-right";
-    const string STATE_BLOCK = "block";
     // Animation Variables
-    private bool isJabbing = false;
-    private bool isRightHitting = false;
-    private bool isDodgingLeft = false;
-    private bool isDodgingRight = false;
-    private bool isBlocking = false;
+    [SerializeField] public string playerState = State.IDLE;
 
     private void OnDisable() {
         jabAction.Disable();
@@ -50,72 +49,63 @@ public class RobotMoves : MonoBehaviour
         jabAction.performed += OnJab;
         rightAction = playerInput.actions["RightDirect"];
         rightAction.performed += OnRight;
-        blockAction = playerInput.actions["Block"];
-        blockAction.performed += OnBlock;
-        blockAction.canceled += OnBlock;
+        // blockAction = playerInput.actions["Block"];
+        // blockAction.performed += OnBlock;
+        // blockAction.canceled += OnBlock;
         dodgeRightAction = playerInput.actions["DodgeRight"];
         dodgeRightAction.performed += OnDodgeRight;
         dodgeLeftAction = playerInput.actions["DodgeLeft"];
         dodgeLeftAction.performed += OnDodgeLeft;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     private bool DoingSomething() {
-        return isJabbing || isRightHitting || isDodgingLeft || isDodgingRight || isBlocking;
+        return !playerState.Equals(State.IDLE);
     }
 
+    // private bool isTeamDoingSomething() {
+    //     return !teamState.Equals(State.IDLE);
+    // }
+
     private void OnJab(InputAction.CallbackContext context) {
-        if (context.ReadValueAsButton() && !DoingSomething() && trainer != null && trainer.isJabbing) {
-            teamState = "jab";
-            isJabbing = true;
-            animator.Play(STATE_JAB);
-            // audioSource.PlayOneShot(punchSound1, volume);
+        if (context.ReadValueAsButton() && !DoingSomething() && trainer != null && trainer.trainerState == State.JAB) {
+            playerState = teamState = State.JAB;
+            animator.Play(State.JAB);
             StartCoroutine(LetAnimationRunForTime(jabSpeed));
+            
+            audioSource.PlayOneShot(punchSound1, volume);
+            
         }
     }
 
     private void OnRight(InputAction.CallbackContext context) {
-        if (context.ReadValueAsButton() && !DoingSomething() && trainer != null && trainer.isRightHitting) {
-            teamState = "right";
-            isRightHitting = true;
-            animator.Play(STATE_RIGHT);
-            // audioSource.PlayOneShot(punchSound2, volume);
+        if (context.ReadValueAsButton() && !DoingSomething() && trainer != null && trainer.trainerState == State.RIGHT) {
+            playerState = teamState = State.RIGHT;
+            animator.Play(State.RIGHT);
             StartCoroutine(LetAnimationRunForTime(jabSpeed));
+            
+            audioSource.PlayOneShot(punchSound2, volume);
         }
     }
 
     private void OnBlock(InputAction.CallbackContext context) {
-        if (context.ReadValueAsButton() && !DoingSomething() && trainer != null && trainer.isBlocking) {
-            teamState = "block";
-            isBlocking = true;
-            animator.Play(STATE_BLOCK);
-        } else {
-            isBlocking = false;
-            animator.Play(STATE_IDLE);
-        }
+        return;
     }
 
     private void OnDodgeRight(InputAction.CallbackContext context) {
-        if (context.ReadValueAsButton() && !DoingSomething() && trainer != null && trainer.isDodgingRight) {
-            teamState = "dodge-right";
-            isDodgingRight = true;
-            animator.Play(STATE_DODGE_RIGHT);
-            // audioSource.PlayOneShot(dodgeSound1, volume);
+        if (context.ReadValueAsButton() && !DoingSomething()) {
+            playerState = teamState = State.DODGE_RIGHT;
+            animator.Play(State.DODGE_RIGHT);
+            audioSource.PlayOneShot(dodgeSound1, volume);
             StartCoroutine(LetAnimationRunForTime(jabSpeed));
         }
     }
 
     private void OnDodgeLeft(InputAction.CallbackContext context) {
-        if (context.ReadValueAsButton() && !DoingSomething() && trainer != null && trainer.isDodgingLeft) {
-            teamState = "dodge-left";
-            isDodgingLeft = true;
-            animator.Play(STATE_DODGE_LEFT);
-            // audioSource.PlayOneShot(dodgeSound2, volume);
+        if (context.ReadValueAsButton() && !DoingSomething()) {
+            playerState = teamState = State.DODGE_LEFT;
+            animator.Play(State.DODGE_LEFT);
+            audioSource.PlayOneShot(dodgeSound2, volume);
             StartCoroutine(LetAnimationRunForTime(jabSpeed));
         }
     }
@@ -123,26 +113,28 @@ public class RobotMoves : MonoBehaviour
     IEnumerator LetAnimationRunForTime(float time)
     {
         yield return new WaitForSeconds(time);
-    
-        // Code to execute after the delay
         BackToIdle();
     }
 
     private void BackToIdle() {
-        if (isBlocking) {
-            animator.Play(STATE_BLOCK);
+        if (playerState == State.BLOCK) {
+            animator.Play(State.BLOCK);
         } else {
-            animator.Play(STATE_IDLE);
+            animator.Play(State.IDLE);
         }
-        isJabbing = false;
-        isRightHitting = false;
-        isDodgingLeft = false;
-        isDodgingRight = false;
-        teamState = "idle";
+        playerState = teamState = State.IDLE;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (trainer.trainerState == State.BLOCK) {
+            playerState = teamState = State.BLOCK;
+            animator.Play(State.BLOCK);
+        }
+        if (trainer.trainerState != State.BLOCK && !(DoingSomething() && !playerState.Equals(State.BLOCK))) {
+            playerState = teamState = State.IDLE;
+            animator.Play(State.IDLE);
+        }
     }
 }
