70662493a4d8551fc3d73efa1a92a9d93c1c9488
barbaraim <birarrazaval1@uc.cl>
2022-11-23 12:44:48 -0300

    add points static vars

--- Assets/Script/StaticVars.cs
+++ Assets/Script/StaticVars.cs
@@ -9,6 +9,20 @@ public class StaticVars : MonoBehaviour
 
     public static int score;
     public static bool win;
+    public static int pointsLeftJabTeam = 100;
+    public static int pointsRightJabTeam = 100;
+    public static int pointsLeftJabMissTeam = -30;
+    public static int pointsRightJabMissTeam = -30;
+    public static int pointsLeftJabBlockTeam = -30;
+    public static int pointsRightJabBlockTeam = -30;
+    public static int pointsLeftJabEnemy = -60;
+    public static int pointsRightJabEnemy = -60;
+    public static int pointsLeftDodgeTeam = 30;
+    public static int pointsRightDodgeTeam = 30;
+    public static int pointsUncoordinateTeam = -50;
+    public static int missEnemy = 10;
+    public static int blockEnemy = 5;
+    public static int pointsBlockTeam;
 
     public static void addPoints(int points)
     {
